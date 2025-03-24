f8b78f8acebccf97ece294713e84ebb24ad3a09c
nullgull <203680049+nullgull@users.noreply.github.com>
2025-03-24 08:44:10 -0700

    Add gitignore

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc aerospace.toml"    # list of files/folders to symlink in homedir
+files="zshrc aerospace.toml gitignore"    # list of files/folders to symlink in homedir
 
 ##########
 
